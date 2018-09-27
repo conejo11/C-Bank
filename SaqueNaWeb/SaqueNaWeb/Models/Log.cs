5e7113cf55eafbac4759c5bfdf6081b17c6e8e6d
conejo11 <gabrielgconejo@gmail.com>
2018-09-27 17:53:04 -0300

    Inicio

---
+++ SaqueNaWeb/SaqueNaWeb/Models/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaqueNaWeb.Models
+{
+	public class Log
+	{
+	    public int Id { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+		public string UserName { get; set; }
+		[Required(ErrorMessage = "Password is required")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
